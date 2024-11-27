be898dd73203e4befa30e74aebcfa3010e51d746
KarnesTH <p_haehnel@hotmail.de>
2024-11-27 17:00:15 +0100

    add first implemenetation of base structure

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,4 @@
-use leptos::leptos_dom::ev::SubmitEvent;
 use leptos::*;
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,59 +7,64 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+#[derive(Debug, Clone)]
+struct Todos {
+    todo_item: Vec<TodoItem>,
+}
+
+#[derive(Debug, Clone)]
+struct TodoItem {
+    title: &'static str,
+    completed: bool,
+    description: &'static str,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = create_signal(String::new());
-    let (greet_msg, set_greet_msg) = create_signal(String::new());
-
-    let update_name = move |ev| {
-        let v = event_target_value(&ev);
-        set_name.set(v);
-    };
-
-    let greet = move |ev: SubmitEvent| {
-        ev.prevent_default();
-        spawn_local(async move {
-            let name = name.get_untracked();
-            if name.is_empty() {
-                return;
-            }
-
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
-            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
-        });
+    let todos = Todos {
+        todo_item: vec![
+            TodoItem {
+                title: "Learn Rust",
+                completed: true,
+                description: "Learn Rust Programming Language",
+            },
+            TodoItem {
+                title: "Learn Leptos",
+                completed: false,
+                description: "Learn Leptos Web Framework",
+            },
+            TodoItem {
+                title: "Learn WebAssembly",
+                completed: false,
+                description: "Learn WebAssembly Programming Language",
+            },
+        ],
     };
 
     view! {
         <main class="container">
-            <h1>"Welcome to Tauri + Leptos"</h1>
+            <h1>"Todo Manager"</h1>
 
             <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-                </a>
+                <button>
+                    "Add Todo"
+                </button>
+                <button>
+                    "Filter"
+                </button>
             </div>
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
 
-            <form class="row" on:submit=greet>
-                <input
-                    id="greet-input"
-                    placeholder="Enter a name..."
-                    on:input=update_name
-                />
-                <button type="submit">"Greet"</button>
-            </form>
-            <p>{ move || greet_msg.get() }</p>
+            <ul>
+                {todos.todo_item.into_iter().map(|todo| view! {
+                    <li>
+                        <div class="row">
+                            <input type="checkbox" checked={todo.completed} />
+                            <h3>{todo.title}</h3>
+                        </div>
+                        <p>{todo.description}</p>
+                    </li>
+                }).collect_view()}
+            </ul>
         </main>
     }
 }
